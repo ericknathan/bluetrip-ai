a670c69de66dd3527b3bb8a7e63c98f6c11636ef
ericknathan <erick.capito@hotmail.com>
2024-06-04 11:00:47 -0300

    feat: add finished app

---
+++ README.md
@@ -0,0 +1,31 @@
+# 🔱 Bluetrip
+
+![Capa](.github/cover.png)
+
+## 💡 Explicação do projeto
+Bluetrip é uma aplicação mobile de turismo focada em oceanos, rios e lagos. O objetivo principal é fornecer uma experiência enriquecedora para turistas e entusiastas da natureza, oferecendo informações detalhadas sobre pontos turísticos, eventos, comércios locais e espécies da fauna e flora aquática. A aplicação inclui:
+
+- **Pontos Turísticos:** Informações sobre locais de interesse próximos a oceanos, rios e lagos, assim como cuidados a se tomar com o local, informações sobre preservação, dentre outros.
+- **Eventos:** Agenda de eventos relacionados aos pontos turisticos.
+- **Comércios:** Listagem de comércios locais, como restaurantes temáticos, lojas de equipamentos de mergulho, etc.
+- **Scanner 3D:** Um scanner que renderiza modelos 3D acompanhados de informações sobre espécies ou partes do ponto turístico.
+- **Identificador de Espécies:** Um classificador de espécies utilizando inteligência artificial para identificar e fornecer informações sobre diferentes espécies aquáticas.
+
+## 👥 Equipe
+Este projeto está sendo desenvolvido pelos seguintes membros:
+
+- RM98110 - André Rohregger Machado (2TDSPV)
+- RM99565 - Erick Nathan Capito Pereira (2TDSPV)
+- RM550841 - Lucas Araujo Oliveira Silva (2TDSPW)
+- RM551886 - Victor Luca do Nascimento Queiroz (2TDSPV)
+- RM99455 - Vinícius Martins Torres Abdala (2TDSPV)
+
+## 💻 Tecnologias
+As principais tecnologias, bibliotecas, ecossistemas e frameworks incluídos no projeto são:
+
+- [Python](https://www.python.org)
+- [Keras](https://keras.io)
+- [Tensorflow](https://www.tensorflow.org)
+- [Numpy](https://numpy.org)
+- [FastAPI](https://fastapi.tiangolo.com)
+- [Uvicorn](https://www.uvicorn.org)
---
+++ app.py
@@ -0,0 +1,66 @@
+
+## Importações
+import io
+
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+from typing import Annotated
+
+from fastapi import FastAPI, File
+from fastapi.middleware.cors import CORSMiddleware
+
+import uvicorn
+
+## Carregamento do modelo gerado pelo Teachable Machine e processo de Labelling
+model = load_model("./model/keras_model.h5", compile=False)
+class_names = open("./model/labels.txt", "r", encoding="utf8").readlines()
+
+## Instanciando o FastAPI
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+## Criação das rotas para conexão com o front-end
+### Rota de status para verificar se a aplicação está sendo executada
+#### [GET] /status
+@app.get("/status")
+async def app_status():
+    return 'ok'
+
+### Rota para predição de imagem enviada no corpo da requisição
+#### [POST] /identify-specie - file: File
+@app.post("/identify-specie")
+async def identify_specie(file: Annotated[bytes, File()]):
+    image = Image.open(io.BytesIO(file)).convert("RGB")
+
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    return {
+        "class_name": class_name[2:].strip(),
+        "confidence_score": round(confidence_score * 100, 3)
+    }
+
+## Execução do servidor HTTP do FastAPI utilizando Uvicorn
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+
+
